a873f3c6b42be4947c795158f6f03d9045edd927
netbrain-cs <l_m_h1027@163.com>
2017-06-21 14:20:29 +0800

    add encode method for class Soundex

--- cpp/soundex/SoundexTest.cpp
+++ cpp/soundex/SoundexTest.cpp
@@ -1,12 +1,20 @@
 #include <gtest/gtest.h>
+#include <string>
 
 class Soundex
 {
-
+public:
+	std::string encode(const std::string & word) {
+		return word;
+	}
 
 };
 
 TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
 {
 	Soundex soundex;
+
+	auto encoded = soundex.encode("A");
+
+	EXPECT_EQ(encoded, "A");
 }
