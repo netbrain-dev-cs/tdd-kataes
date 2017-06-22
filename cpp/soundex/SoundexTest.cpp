dc599b0e30bdbf75b6dbc260c274f3a31ee82ef0
netbrain-cs <l_m_h1027@163.com>
2017-06-22 20:09:05 +0800

    add encodeDigists

--- cpp/soundex/SoundexTest.cpp
+++ cpp/soundex/SoundexTest.cpp
@@ -1,24 +1,42 @@
 #include <gtest/gtest.h>
 #include <string>
+#include <map>
 
 class Soundex
 {
 public:
-	std::string encode(const std::string & word) {
-		auto  encoded = word.substr(0, 1);
-		if (word.size() > 1) {
-			encoded += "1";
-		}
+	static const size_t MAX_CODE_LENGTH = 4;
 
-		return zeroPad(encoded);
+	std::string encode(const std::string & word) {
+		return zeroPad(head(word) + encodeDigits(word));
 	}
+
+	std::string head(const std::string & word) {
+		return word.substr(0, 1);
+	}
+
+	std::string encodeDigits(const std::string & word) {
+		if (word.size() > 1) return encodeDigit(word[1]);
+		return "";
+	}
+
+	std::string encodeDigit(const char letter) {
+		static const std::map<char, std::string > encodings = {
+			{'b',"1"},{'f',"1"},{'p',"1"},{'v',"1"},
+			{'c',"2"},{'g',"2" }, { 'j',"2" }, { 'k',"2" }, { 'q',"2" },{'s',"2"},{'x',"2"},{'z',"2"},
+			{ 'd',"3" }, { 't',"3" },
+			{ 'l',"4" },
+			{ 'm',"5" }, { 'n',"5" },
+			{ '4',"6" }
+		};
+		return encodings.find(letter)->second;
+	}
+
 
 	std::string zeroPad(const std::string & word) {
-		auto zerosNeeded = 4 - word.size();
+		auto zerosNeeded = MAX_CODE_LENGTH - word.size();
 		return word + std::string(zerosNeeded, '0');
 	}
-
-
 };
 
 class SoundexEncoding :public testing::Test {
@@ -40,4 +58,5 @@ TEST_F(SoundexEncoding, PadsWithZeroToEnsureThreeDigits)
 
 TEST_F(SoundexEncoding, RetainSoleLetterOfTwoLetterWord) {
 	EXPECT_EQ(soundex.encode("Ab"), "A100");
+	EXPECT_EQ(soundex.encode("Ac"), "A200");
 }
